3f88fe5223cd40f280657c582c635e87c3a7814c
FallenWyvern <b.holmes.29@gmail.com>
2020-07-09 09:12:46 -0400

    Create acolyte.mm

---
+++ StatBlocks/acolyte.mm
@@ -0,0 +1,62 @@
+<stat-block>
+    <creature-heading>
+      <h1>Acolyte</h1>
+      <h2>Medium humanoid (any race), any alignment</h2>
+    </creature-heading>
+
+    <top-stats>
+      <property-line>
+        <h4>Armor Class</h4>
+        <p>10</p>
+      </property-line>
+      <property-line>
+        <h4>Hit Points</h4>
+        <p>9 (2d8)</p>
+      </property-line>
+      <property-line>
+        <h4>Speed</h4>
+        <p>30 ft.</p>
+      </property-line>
+
+      <abilities-block data-str="10"
+                       data-dex="10"
+                       data-con="10"
+                       data-int="10"
+                       data-wis="14"
+                       data-cha="11"></abilities-block>
+
+      <property-line>
+        <h4>Skills</h4>
+        <p>Medicine +4, Religion +2</p>
+      </property-line>
+      <property-line>
+        <h4>Senses</h4>
+        <p>passive Perception 12</p>
+      </property-line>
+      <property-line>
+        <h4>Languages</h4>
+        <p>Any one language (usually Common)</p>
+      </property-line>
+      <property-line>
+        <h4>Challenge</h4>
+        <p>1/4 (50 XP)</p>
+      </property-line>
+    </top-stats>
+
+    <property-block>
+      <h4>Spellcasting.</h4>
+      <p>Spellcasting. The acolyte is a 1st-level spellcaster. Its spellcasting ability is 
+      Wisdom (spell save DC 12, +4 to hit with spell attacks). The acolyte has following 
+      cleric spells prepared:</p>
+      <p>Cantrips (at will): <i>light</i>, <i>sacred flame</i>, <i>thaumaturgy</i></p>
+      <p>1st level (3 slots): <i>bless</i>, <i>cure wounds</i>, <i>sanctuary</i></p>
+    </property-block>
+    
+    <h3>Actions</h3>
+
+    <property-block>
+      <h4>Club.</h4>
+      <p><i>Melee Weapon Attack:</i> +2 to hit, reach 5 ft., one target.
+        <i>Hit:</i> 2 (1d4) bludgeoning damage.</p>
+    </property-block>
+  </stat-block>
