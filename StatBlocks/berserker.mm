ec77064be2d0a83e33aecf9200e4d9f8e038c21d
FallenWyvern <b.holmes.29@gmail.com>
2020-11-19 15:59:10 +0000

    And included them now

---
+++ StatBlocks/berserker.mm
@@ -0,0 +1,85 @@
+<stat-block>
+    <creature-heading>
+      <h1><span id='mmName'>Creature Name</span></h1>
+      <h2><span id='mmSize'>Medium</span> humanoid (<span id='mmRace'>any race</span>), any alignment</h2>
+    </creature-heading>
+
+    <top-stats>
+      <property-line>
+        <h4>Armor Class</h4>
+        <p id='mmAC'>10</p>
+      </property-line>
+      <property-line>
+        <h4>Hit Points</h4>        
+        <p id='mmHP'>aa (xdy + z)</p>        
+      </property-line>
+      <property-line>
+        <h4>Speed</h4>
+        <p id='mmSpeed'><span id='mmBaseSpeed'>30</span> ft.<span id='mmExtraSpeed'></span></p>        
+        </span>
+      </property-line>
+      <div id='mmStats'>
+        <abilities-block data-str="10"
+                        data-dex="10"
+                        data-con="10"
+                        data-int="10"
+                        data-wis="10"
+                        data-cha="10"></abilities-block>
+      </div>
+      <property-line id='mmSkills'>
+        <h4>Skills</h4>
+        <p></p>
+      </property-line>
+      <property-line id='mmSaves'>                     
+        <h4>Saving Throws</h4>
+        <p></p>
+      </property-line>
+      <property-line id='mmVul'>
+      </property-line>
+      <property-line id='mmResistance'>
+      </property-line>
+      <property-line id='mmImmunity'>
+      </property-line>
+      <property-line id='mmCImmunity'>
+      </property-line>
+      <property-line id='mmSenses'>
+        <h4>Senses</h4>
+        <p></p>
+      </property-line>
+      <property-line id='mmLanguage'>
+        <h4>Languages</h4>
+        <p>Any one language (usually Common)</p>
+      </property-line>
+      <property-line>
+        <h4>Challenge</h4>        
+        <p id="mmCR">1/4 (50 XP)</p>
+      </property-line>
+    </top-stats>
+
+    <div id = 'mmAbilities'>        
+        <property-block>        
+                <h4>Spellcasting.</h4>
+                <p id='mmSpellcasting' data-level="1st" data-ability="Wisdom" data-spelllist="cleric">                
+                </p>
+                <p>Cantrips (at will): <i>light</i>, <i>sacred flame</i>, <i>thaumaturgy</i></p>
+                <p>1st level (3 slots): <i>bless</i>, <i>cure wounds</i>, <i>sanctuary</i></p>        
+        </property-block>      
+        <property-block>        
+                <h4>Title.</h4>
+                <p>Details</p>        
+        </property-block>      
+    </div>
+    
+    <h3>Actions</h3>
+    <div id = 'mmAttacks'>
+        <property-block>
+        <h4>Club.</h4>
+        <p><i>Melee Weapon Attack:</i> +2 to hit, reach 5 ft., one target.
+            <i>Hit:</i> 2 (1d4) bludgeoning damage.</p>
+        </property-block>
+    </div>
+    <property-block>        
+                <h4>Title.</h4>
+                <p>Details</p>        
+    </property-block>      
+  </stat-block>
