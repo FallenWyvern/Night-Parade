a3e1c0a7772467f959caea0bc67a7549d090feac
FallenWyvern <b.holmes.29@gmail.com>
2021-10-26 13:30:38 +0000

    Added harpy

---
+++ StatBlocks/harpy.mm
@@ -0,0 +1,83 @@
+<stat-block>
+    <creature-heading>
+      <h1><span id='mmName'>Harpy</span></h1>
+      <h2><span id='mmSize'>Medium</span> <span id='mmType'>monstrosity</span> <span id='mmRace'></span>, chaotic evil</h2>
+    </creature-heading>
+
+    <top-stats>
+      <property-line>
+        <h4>Armor Class</h4>
+        <p id='mmAC'>11</p>
+      </property-line>
+      <property-line>
+        <h4>Hit Points</h4>        
+        <p id='mmHP'>38 (7d8 + 7)</p>        
+      </property-line>
+      <property-line>
+        <h4>Speed</h4>
+        <p id='mmSpeed'><span id='mmBaseSpeed'>20</span> ft.<span id='mmExtraSpeed'>fly 40 ft.</span></p>        
+        </span>
+      </property-line>
+      <div id='mmStats'>
+        <abilities-block data-str="12"
+                        data-dex="13"
+                        data-con="12"
+                        data-int="7"
+                        data-wis="10"
+                        data-cha="13"></abilities-block>
+      </div>
+      <property-line id='mmSkills'>
+        <h4>Skills</h4>
+        <p></p>
+      </property-line>
+      <property-line id='mmSaves'>                     
+        <h4>Saving Throws</h4>
+        <p></p>
+      </property-line>
+      <property-line id='mmVul'>
+      </property-line>
+      <property-line id='mmResistance'>
+      </property-line>
+      <property-line id='mmImmunity'>
+      </property-line>
+      <property-line id='mmCImmunity'>
+      </property-line>
+      <property-line id='mmSenses'>
+        <h4>Senses</h4>
+        <p></p>
+      </property-line>
+      <property-line id='mmLanguage'>
+        <h4>Languages</h4>
+        <p>Common</p>
+      </property-line>
+      <property-line>
+        <h4>Challenge</h4>        
+        <p id="mmCR">1 (200 XP)</p>
+      </property-line>
+    </top-stats>
+
+    <div id = 'mmAbilities'>                     
+    </div>
+    
+    <h3>Actions</h3>
+    <property-block>        
+                <h4>Multiattack.</h4>
+                <p>The harpy makes two attacks: one with its claws and one with its club.</p>        
+    </property-block> 
+    <div id = 'mmAttacks'>
+        <property-block>
+        <h4>Claws.</h4>
+        <p><i>Melee Weapon Attack:</i> +3 to hit, reach 5 ft., one target.
+            <i>Hit:</i> 6 (2d4 + 1) slashing damage.</p>
+        </property-block>
+        <property-block>
+        <h4>Club.</h4>
+        <p><i>Melee or Ranged Weapon Attack:</i> +3 to hit, reach 5 ft. or range 20/60 ft.,, one target.
+            <i>Hit:</i> 3 (1d4 + 1) bludgeoning damage.</p>
+        </property-block>
+    </div>
+    <property-block>        
+                <h4>Luring Song.</h4>
+                <p>The harpy sings a magical melody. Every humanoid and giant within 300 feet of the harpy that can hear the song must succeed on a DC 11 Wisdom saving throw or be charmed until the song ends. The harpy must take a bonus action on its subsequent turns to continue singing. It can stop singing at any time. The song ends if the harpy is incapacitated. While charmed by the harpy, a target is incapacitated and ignores the songs of other harpies. If the charmed target is more than 5 feet away from the harpy, the target must move on its turn toward the harpy by the most direct route, trying to get within 5 feet. It doesn't avoid opportunity attacks, but before moving into damaging terrain, such as lava or a pit, and whenever it takes damage from a source other than the harpy, the target can repeat the saving throw. A charmed target can also repeat the saving throw at the end of each of its turns. If the saving throw is successful, the effect ends on it. A target that successfully saves is immune to this harpy's song for the next 24 hours.</p>        
+    </property-block>      
+  </stat-block>
